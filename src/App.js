e33ae2c230452b98c9a1f70ec05b2c6ef6df6244
milad karimi <izi.bizines@gmail.com>
2022-06-28 14:30:43 +0430

    026 Monsters Rolodex - Searching & Filtering

--- src/App.js
+++ src/App.js
@@ -35,6 +35,18 @@ class App extends Component {
           placeholder="search monsters"
           onChange={event => {
             console.log(event.target.value);
+
+            const searchString = event.target.value.toLowerCase();
+
+            const filteredMonsters = this.state.monsters.filter(
+              monster => {
+                return monster.name.toLowerCase().includes(searchString);
+              });
+
+            this.setState(
+              () => {
+                return { monsters: filteredMonsters };
+              });
           }}
         />
 
