efda4e920d6bee824d37f6d93a92fcec36d7e7b0
milad karimi <izi.bizines@gmail.com>
2022-06-28 19:18:56 +0430

    032 Monsters Rolodex - Component Props

--- src/App.js
+++ src/App.js
@@ -52,19 +52,7 @@ class App extends Component {
           onChange={onSearchChange}
         />
 
-        {/* {
-          filteredMonsters.map(
-            monster => {
-              return (
-                <div key={monster.id}>
-                  <h1>{monster.name}</h1>
-                </div>
-              );
-            }
-          )
-        } */}
-
-        <CardList />
+        <CardList monsters={filteredMonsters} />
 
       </div>
     );
